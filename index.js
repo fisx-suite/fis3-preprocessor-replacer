b3b8ca39746d79ba0e45fbd1f85629899501a3bb
wuhy <sparklewhy@gmail.com>
2016-12-24 21:17:24 +0800

    add envify support

--- index.js
+++ index.js
@@ -5,6 +5,19 @@
 
 'use strict';
 
+/**
+ * 基于字符串匹配方式替换环境变量
+ *
+ * @param {string} content 文件内容
+ * @param {Object} options 替换选项
+ * @param {boolean} options.isProd 是否是生产环境
+ * @return {string}
+ */
+function envify(content, options) {
+    var env = options.isProd ? 'production' : 'development';
+    return content.replace(/process\.env\.NODE_ENV/g, '\'' + env + '\'');
+}
+
 module.exports = function (content, file, conf) {
     if (!file.isText()) {
         return content;
@@ -16,10 +29,19 @@ module.exports = function (content, file, conf) {
         from = new RegExp(_.escapeReg(from), 'g');
     }
 
-    if (!_.isRegExp(from, 'RegExp')) {
+    if (from && !_.isRegExp(from, 'RegExp')) {
         fis.log.error('fis3-preprocessor-replacer: option.from must a string or RegExp.');
     }
+    else if (from) {
+        content = content.replace(from, conf.to);
+    }
 
-    return content.replace(from, conf.to);
+    if (conf.envify) {
+        content = envify(content, {
+            isProd: conf.isProd
+        });
+    }
+
+    return content;
 };
 
